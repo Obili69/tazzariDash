a1d190fcebaa868c99b03f99c9c62de0784a6a74
Obili69 <christian.magnus@obrecht.ch>
2025-09-08 17:19:48 +0200

    Update lv_conf.h

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -121,8 +121,8 @@
     #define LV_USE_MSGBOX 0
     #define LV_USE_SPINBOX 0
     #define LV_USE_SPINNER 0
-    #define LV_USE_TABVIEW 0
-    #define LV_USE_TILEVIEW 0
+    #define LV_USE_TABVIEW 1
+    #define LV_USE_TILEVIEW 1
     #define LV_USE_WIN 0
     
     // Optimize theme for speed
